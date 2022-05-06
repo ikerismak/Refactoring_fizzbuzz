65ffd7abbb8585acba6a0b885a254164c1cc75b1
Iker Toscano <ikerismak@gmail.com>
2022-05-06 00:41:34 -0500

    adding explorerservice and filterbymission metod

--- Index.js
+++ Index.js
@@ -1,4 +1,11 @@
 const Reader = require('./lib/utils/Reader.js')
+const ExplorerService = require("./lib/services/ExplorerService.js");
+
 
 const explorer = Reader.readJsonFile("explorers.json")
 console.log(explorer);
+
+
+const explorersInNode = ExplorerService.filterByMission(explorer,"node")
+
+console.log(explorersInNode.length);
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
 // Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+// const rawdata = fs.readFileSync("explorers.json");
+// const explorers = JSON.parse(rawdata);
 // console.log(explorers);
 
 
